c07d15e5e484c997ba66cafd26eadef63d9e1b2f
DanielRosandic <48002561+Rosandic21@users.noreply.github.com>
2023-08-24 05:34:41 -0400

    updated app.js to handle initial route

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
 import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import React, {useEffect} from 'react'
 import Header from './components/Header';
 import Homepage from './Pages/Homepage';
 import CoinPage from './Pages/CoinPage';
 
 
 function App() {
+  useEffect(() => {
+    // Redirect to the root route on initial load
+    if (window.location.pathname === '/CryptoTrackerApp/') {
+      window.location.href = '/';
+    }
+  }, []);
 
     return (
       <BrowserRouter>
       <Header />
       <Routes>
-            <Route path="/" element={<Homepage />} />
-            <Route path="/coin/:id" element={<CoinPage />} />
+          <Route path="/" element={<Homepage />} />
+          <Route path="/coin/:id" element={<CoinPage />} />
       </Routes>
       </BrowserRouter>
     );
