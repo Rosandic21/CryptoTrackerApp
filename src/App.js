7249e2acbdacf1eebd505561d076a8e38f924dcc
DanielRosandic <48002561+Rosandic21@users.noreply.github.com>
2023-08-24 07:16:58 -0400

    click CryptoUpdates icon to be directed to webpage

--- src/App.js
+++ src/App.js
@@ -1,19 +1,11 @@
 import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import React, {useEffect} from 'react'
 import Header from './components/Header';
 import Homepage from './Pages/Homepage';
 import CoinPage from './Pages/CoinPage';
 
 
 function App() {
-  useEffect(() => {
-    // Redirect to the root route on initial load
-    if (window.location.pathname === '/CryptoTrackerApp/') {
-      window.location.href = '/';
-    }
-  }, []);
-
     return (
       <BrowserRouter>
       <Header />
